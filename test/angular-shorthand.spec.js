7f77353bcf70e972ce0c8564acf46347fd9b189c
Ben Lesh <ben@benlesh.com>
2014-01-23 09:36:12 -0500

    added tests around service creation

--- test/angular-shorthand.spec.js
+++ test/angular-shorthand.spec.js
@@ -1,8 +1,8 @@
-describe('module creation', function (){
+describe('module creation', function () {
 
     var mockModule;
 
-    beforeEach(function(){
+    beforeEach(function () {
         mockModule = {
             factory: jasmine.createSpy('module.factory'),
             provider: jasmine.createSpy('module.provider'),
@@ -18,46 +18,73 @@ describe('module creation', function (){
     describe('when ng("myApp", ["dependency"]) is called', function () {
         var mod;
 
-        beforeEach(function (){
+        beforeEach(function () {
             mod = ng('myApp', ["dependency"]);
         });
 
-        it('should call angular.module("myApp", ["dependency"])', function (){
+        it('should call angular.module("myApp", ["dependency"])', function () {
             expect(angular.module).toHaveBeenCalledWith('myApp', ['dependency']);
         });
 
-        it('should return a shorthand function', function (){
+        it('should return a shorthand function', function () {
             expect(typeof mod).toBe('function');
         });
     });
 
-    describe('when ng("myApp") is called', function (){
+    describe('when ng("myApp") is called', function () {
         var mod;
 
-        beforeEach(function() {
+        beforeEach(function () {
             mod = ng('myApp');
         });
 
-        it('should call angular.module("myApp")', function (){
+        it('should call angular.module("myApp")', function () {
             expect(angular.module).toHaveBeenCalledWith('myApp');
         });
 
-        it('should return a shorthand function', function (){
+        it('should return a shorthand function', function () {
             expect(typeof mod).toBe('function');
         });
     });
 
-    describe('ng("myApp", "serviceName", arg)', function (){
+    describe('ng("myApp", "lowerCamelName", arg)', function () {
         describe('when arg is a function', function () {
             var arg;
 
-            beforeEach(function (){
-                arg = function() {};
-                ng('myApp', 'serviceName', arg);
+            beforeEach(function () {
+                arg = function () {
+                };
+                ng('myApp', 'lowerCamelName', arg);
             });
 
-            it('should call module.factory("serviceName", arg)', function () {
-                expect(mockModule.factory).toHaveBeenCalledWith('serviceName', arg);
+            it('should call module.factory("lowerCamelName", arg)', function () {
+                expect(mockModule.factory).toHaveBeenCalledWith('lowerCamelName', arg);
+            });
+        });
+
+        describe('when arg is an Array', function () {
+            var arg;
+
+            beforeEach(function () {
+                arg = [];
+                ng('myApp', 'lowerCamelName', arg);
+            });
+
+            it('should call module.factory("lowerCamelName", arg)', function () {
+                expect(mockModule.factory).toHaveBeenCalledWith('lowerCamelName', arg);
+            });
+        });
+
+        describe('when arg is an Object', function (){
+            var arg;
+
+            beforeEach(function () {
+                arg = {};
+                ng('myApp', 'lowerCamelName', arg);
+            });
+
+            it('should call module.value("lowerCamelName", arg)', function () {
+                expect(mockModule.value).toHaveBeenCalledWith('lowerCamelName', arg);
             });
         });
     });
