76e6dd07b721b10ad6c29dcdfc1637cce668eb4f
Ben Lesh <ben@benlesh.com>
2014-01-22 23:05:54 -0500

    added uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,18 @@
 module.exports = function(grunt) {
 
-    grunt.config({
-        pkg: grunt.util.readJSON('package.json')
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                    '  (c) 2014 Ben Lesh - http://www.benlesh.com - MIT license */'
+            },
+            my_target: {
+                files: {
+                    'dest/angular-shorthand.min.js': ['src/angular-shorthand.js']
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
